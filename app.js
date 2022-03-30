6aa6873246604e1d27e9472ad9fe2a1f6cda9713
FernandoDanielZigarra <fernando.zigarra@gmail.com>
2022-03-30 12:00:21 -0300

    Register&LoginAdded

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("C14-grupo 5")
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = 3030;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    return res.sendFile(path.resolve(__dirname,'views','index.html'))
+});
+app.get('/register', (req,res) => {
+    return res.sendFile(path.resolve(__dirname, 'views', 'register.html'))
+});
+app.get('/login', (req,res) => {
+    return res.sendFile(path.resolve(__dirname, 'views', 'login.html'))
+});
+
+app.listen(port, () => console.log(`Server running in port http://localhost:${port}`))
