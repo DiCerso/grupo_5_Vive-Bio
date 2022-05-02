43b7f2057025a21f6d5315ae678e29b69c945d89
MayraCortinez <mayrautopia@gmail.com>
2022-05-02 20:03:37 -0300

    corrijo nombre product.js - elimino archivos de productDetail

--- site_ViveBio/app.js
+++ site_ViveBio/app.js
@@ -44,12 +44,8 @@ app.get('/productCard', (req,res) => {
     return res.sendFile(path.resolve(__dirname, 'views', 'productCard.html'))
 });
 
-app.get('/productdetail', (req,res) => {
-    return res.sendFile(path.resolve(__dirname, 'views', 'productDetail.html'))
-});
-
 app.get('/carrito', (req,res) => {
     return res.sendFile(path.resolve(__dirname, 'views', 'productCart.html'))
 });
 
-app.listen(port, () => console.log(`Server running in port http://localhost:${port}`))
+app.listen(port, () => console.log(`Server running in port http://localhost:${port}`));
