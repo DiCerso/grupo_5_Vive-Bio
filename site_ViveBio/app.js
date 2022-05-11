b417f003154d184102491579d5935019c24c87c3
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-05-11 18:39:30 -0300

    Entry point set

--- site_ViveBio/app.js
+++ site_ViveBio/app.js
@@ -1,26 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
+app.use(express.static(path.join(__dirname, '..', 'public')));
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+/*rutas dinámicas */
+const indexRouter = require('./routes/index');
+const productsRouter = require('./routes/product');
+const usersRouter = require('./routes/users')
+
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
