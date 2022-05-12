2e98514ce918d49d0b2a69ba744d461df07e2b23
MayraCortinez <mayrautopia@gmail.com>
2022-05-12 00:19:31 -0300

    add data

--- site_ViveBio/src/controllers/productController.js
+++ site_ViveBio/src/controllers/productController.js
@@ -1,13 +1,14 @@
+const products = require('../data/products');
 const path = require('path');
 
 module.exports = {
-    Card : (req,res) => res.render(path.resolve('./src/views/products/productCard')),
-        /*
+    Card : (req,res) => {
         const {idProduct} = req.params;
+
         const product = products.find(product => product.id === +idProduct);
-        return res.render('productCard',{
-            product
-        })*/
+        return res.render(path.resolve('./src/views/products/productCard', {product}))
+    },
+    
 
     All : (req,res) => res.render(path.resolve('./src/views/products/productAll')),
     Cart : (req,res) => res.render(path.resolve('./src/views/products/productCart')),
---
+++ site_ViveBio/src/data/products.js
@@ -0,0 +1,146 @@
+module.exports = { 
+    {
+        id : 1,
+        name : "Jabón Citrus, Café y Cedrón",
+        name-cards : "" ,
+        description : ,
+        category : "BioCorporal",
+        property : "Exfoliante",
+        price : 300,
+        volume : 90,
+        discount: 30,
+        image : 
+    },
+    {
+        id : 2,
+        name : "Jabón Aloe Vera, Rosas y Manzanilla",
+        description : ,
+        category : "BioCorporal",
+        property : "Humectante",
+        price : 300,
+        volume : 90,
+        discount: 20, 
+        image : 
+    },
+    {
+        id : 3,
+        name : "Jabón Aloe Vera, Lavanda y Romero",
+        description : ,
+        price : 300,
+        category : "BioCorporal",
+        property : "Humectante",
+        volume : 90,
+        discount: 20,
+        image : 
+    },
+    {
+        id : 4,
+        name : "Jabón Menta, Tea Three y Caléndula",
+        description : ,
+        price : 300,
+        category : "BioCorporal",
+        property : "Antiséptico",
+        volume : 90,
+        discount: 40,
+        image : 
+    },
+    {
+        id : 5,
+        name : "Shampoo Aceite de Argán",
+        description : ,
+        price : 400,
+        category : "BioCapilar",
+        property : "Humectante",
+        volume : 90,
+        discount: 0,
+        image : 
+    },
+    {
+        id : 6,
+        name : "Shampoo Aceite de Coco",
+        description : ,
+        price : 400,
+        category : "BioCapilar",
+        property : "Hidratante",
+        volume : 90,
+        discount: 0,
+        image : 
+    },
+    {
+        id : 7,
+        name : "Acondicionador Aceite de Argán",
+        description : ,
+        price : 400,
+        category : "BioCapilar",
+        property : "Humectante",
+        volume : 90,
+        discount: 0,
+        image : 
+    },
+    {
+        id : 8,
+        name : "Acondicionador Aceite de Coco",
+        description : ,
+        price : 400,
+        category : "BioCapilar",
+        property : "Hidratante"
+        volume : 90,
+        discount: 0,
+        image: 
+    },
+    {
+        id : 9,
+        name : "Shampoo y Acondicionador Almendras",
+        description : ,
+        price : 600,
+        category : "BioCapilar",
+        property : "Humectante"
+        volume : 120,
+        discount: 20,
+        image: 
+    },
+    {
+        id : 10,
+        name : "Shampoo y Acondicionador Karité",
+        description : ,
+        price : 600,
+        category : "BioCapilar",
+        property : "Hidratante"
+        volume : 120,
+        discount: 20,
+        image: 
+    },
+    {
+        id : 11,
+        name : "Shampoo y Acondicionador Salvia",
+        description : ,
+        price : 600,
+        category : "BioCapilar",
+        property : "Revitalizante",
+        volume : 120,
+        discount: 20,
+        image: 
+    },
+    {
+        id : 12,
+        name : "Luffa y Jabón Naranja y Café + Jabonera",
+        description : ,
+        price : 800,
+        category : "BioSpa",
+        property : "Exfoliante",
+        volume : 140,
+        discount: 25,
+        image : 
+    },
+    {
+        id : 13,
+        name : "Luffa y Jabón Té Verde + Jabonera",
+        description : ,
+        price : 800,
+        category : "BioSpa",
+        property : "Hidratante",
+        volume : 140,
+        discount: 25,
+        image : 
+    },
+  }
