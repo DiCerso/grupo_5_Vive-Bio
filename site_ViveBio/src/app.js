47f36311cd28f8f333dd2822f63405a97894a74e
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-05-21 19:19:43 -0300

    app.js fixed

--- site_ViveBio/src/app.js
+++ site_ViveBio/src/app.js
@@ -7,11 +7,11 @@ const methodOverride = require('method-override');
 
 
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'..','public')));
+app.set('views', path.join(__dirname, 'views'));
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,'..','public')));
 
 //manipulacion JSON
 app.use(express.json());
