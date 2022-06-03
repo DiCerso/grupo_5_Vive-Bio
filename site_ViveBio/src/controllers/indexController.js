313912308d4f0880bc077220f9c0f217e415780b
DiCerso <lautaro_cer@hotmail.com>
2022-06-02 22:44:46 -0300

    semi-carrusel

--- site_ViveBio/src/controllers/indexController.js
+++ site_ViveBio/src/controllers/indexController.js
@@ -6,9 +6,27 @@ const products = require('../data/products')
 
 module.exports = {
     index : (req, res) =>{
+        let product1 = [], product2 = [], i;
+
+        const product = products.filter(product => {
+            if(product.dest == true){
+                return product
+            }
+        });
+
+        
+        
+        for(i = 0; i <= 3; i++){
+            product1.push(product[i]);
+        }
+        for(i = 4; i <= 7; i++){
+            product2.push(product[i]);
+        }
+
 
         return res.render('index', {
-            products,
+            product1,
+            product2,
             category
         })
     }
