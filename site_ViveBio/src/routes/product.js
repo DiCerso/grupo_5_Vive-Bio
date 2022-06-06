643c63de0d3666b0259ef00106097ecf82f2baf2
MayraCortinez <mayrautopia@gmail.com>
2022-06-06 15:16:58 -0300

    sprint 5 - adjustment admin, price, view products

--- site_ViveBio/src/routes/product.js
+++ site_ViveBio/src/routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const upload =require('../middlewares/uploadImageProduct');
+const adminCheck = require('../middlewares/adminCheck');
 
 const {add, edit, update, store, Card, All, remove, search} = require('../controllers/productController');
 
@@ -8,11 +9,11 @@ const {add, edit, update, store, Card, All, remove, search} = require('../contro
 /*Products*/
 router.get('/Card/:id', Card);
 router.get('/All', All);
-router.get('/add', add);
+router.get('/add', adminCheck, add);
 router.post('/add',upload.array('image'), store);
-router.get('/edit/:id', edit);
-router.put('/update/:id',upload.array('image'),update)
-router.delete("/remove/:id", remove)
+router.get('/edit/:id', adminCheck, edit);
+router.put('/update/:id',upload.array('image'),update);
+router.delete("/remove/:id",adminCheck, remove);
 router.get("/search", search);
 
 
