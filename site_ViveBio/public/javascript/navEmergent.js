50da247efd931dedb0c64193bfc6579d88595011
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-07-01 15:01:53 -0300

    Add navEmergent in javascript

---
+++ site_ViveBio/public/javascript/navEmergent.js
@@ -0,0 +1,11 @@
+const btnNavMobile = document.querySelector("#btnNavMob");
+const emergente = document.querySelector("#emergente");
+const solapa1 = document.querySelector("#solapa1");
+const solapa2 = document.querySelector("#solapa2");
+
+btnNavMobile.addEventListener("click",function(){
+    emergente.classList.toggle("show_nav_mobile")
+    solapa1.classList.toggle("active1")
+    solapa2.classList.toggle("active2")
+    btnNavMobile.classList.toggle("active3")
+});
