6d0eeb5ead1a513fe539dbd94b3af24e69f69441
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-07-05 22:54:56 -0300

    fix

--- site_ViveBio/src/database/models/cart.js
+++ site_ViveBio/src/database/models/cart.js
@@ -3,56 +3,80 @@ module.exports = (sequelize, DataTypes) => {
     const alias = 'Cart'
 
     const cols = {
-        id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true
+        id : {
+            type : DataTypes.INTEGER.UNSIGNED,
+            autoIncrement : true,
+            allowNull : false,
+            primaryKey : true
         },
-        user_id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            allowNull: false,
+        user_id : {
+            type : DataTypes.INTEGER.UNSIGNED,
+            allowNull : false,
             references: {
                 model: {
-                    tableName: 'users'
+                    tableName : 'users'
                 },
                 key: 'id',
             }
         },
-        product_id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            allowNull: false,
+        product_id : {
+            type : DataTypes.INTEGER.UNSIGNED,
+            allowNull : false,
             references: {
                 model: {
-                    tableName: 'products'
+                    tableName : 'products'
                 },
                 key: 'id',
             }
         },
-        cant: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            allowNull: false,
-            default: 1
+        payment_id : {
+            type : DataTypes.INTEGER.UNSIGNED,
+            allowNull : false,
+            references: {
+                model: {
+                    tableName : 'payments'
+                },
+                key: 'id',
+            }
+        },
+        order_id : {
+            type : DataTypes.INTEGER.UNSIGNED,
+            allowNull : false,
+            references: {
+                model: {
+                    tableName : 'orders'
+                },
+                key: 'id',
+            }
         }
     }
 
     const config = {
-        tableName: 'carts',
-        timestamps: false
+        tableName : 'carts',
+        timestamps : false
     }
 
 
     const Cart = sequelize.define(alias, cols, config)
 
-    Cart.associate = function (models) {
-        Cart.belongsTo(models.Product, {
-            as: 'product',
-            foreignKey: 'product_id'
+    Cart.associate = function(models){
+        Cart.belongsTo(models.Product,{
+           as : 'product',
+           foreignKey : 'product_id'
         })
-        Cart.belongsTo(models.User, {
-            as: 'user',
-            foreignKey: 'user_id'
+        Cart.belongsTo(models.User,{
+            as : 'user',
+            foreignKey : 'user_id'
         })
+        Cart.belongsTo(models.Payment,{
+            as : 'payment',
+            foreignKey : 'payment_id'
+        })
+        Cart.belongsTo(models.Order,{
+            as : 'order',
+            foreignKey : 'order_id'
+        })
+
     }
 
     return Cart
