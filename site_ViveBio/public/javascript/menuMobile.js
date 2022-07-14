6719fb519a5013efb8a64ea08790a5708d9a5dd2
FernandoDanielZigarra <99737822+FernandoDanielZigarra@users.noreply.github.com>
2022-07-14 02:26:30 -0300

    Change navMobile

--- site_ViveBio/public/javascript/menuMobile.js
+++ site_ViveBio/public/javascript/menuMobile.js
@@ -1,7 +1,9 @@
 const btnMenu = document.querySelector("#btnMenu");
 const menu = document.querySelector("#menu");
-/* var body = document.querySelector("body"); */
-
+const burger = document.querySelector("#burger")
+const navclose = document.querySelector('.navClose')
 btnMenu.addEventListener("click",function(){
+    burger.classList.toggle('active')
+    navclose.classList.toggle('active')
     menu.classList.toggle("mostrar")  
 });
