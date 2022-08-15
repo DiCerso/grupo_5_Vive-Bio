fce3ceceef5723729b899b7ba2d689924a032b2e
MayraCortinez <mayrautopia@gmail.com>
2022-08-15 01:21:36 -0300

    add script ViveBio.sql

--- site_ViveBio/src/database/config/config.js
+++ site_ViveBio/src/database/config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: "root",
+    password: "root",
+    database: "vivebio_db",
+    host: "127.0.0.1",
     dialect: "mysql",
-    port : process.env.DB_PORT
+    port : 3306
   },
   test: {
     username: "root",
